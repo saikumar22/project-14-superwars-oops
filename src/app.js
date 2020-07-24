5f08fd1929507269dc6f2326240bf0c776c65b2e
saikumar22 <saigoud2212@gmail.com>
2020-07-24 09:14:42 +0530

    FSJ0620D102

--- src/app.js
+++ src/app.js
@@ -26,7 +26,11 @@ class Player {
     constructor(id, name, type) {
         // Create member variables and assign values
         // Type your code
-
+        this.id =id;
+        this.name=name;
+        this.image="images/super-" + (id+1) + ".png";
+        this.strenth = this.getRandomStrength();
+        this.type = type;
     }
 
     // getting random strength
@@ -38,8 +42,17 @@ class Player {
     view = () => {
         // Accumulate HTML template
         // Type your code here
-
-    
+        let player = document.createElement('div');
+        player.classList.add('player');
+        player.setAttribute('data-id', this.id);
+        let image = document.createElement('img');
+        image.setAttribute('src', this.image);
+        let name = document.createElement('div');
+        name.textContent = this.name;
+        let strength = document.createElement('div');
+        strength.textContent = this.strength;
+        strength.className = 'strength';
+        player.append(image, name, strength);
         return player;
     }
 }
@@ -50,7 +63,10 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
-
+    this.players = players.map((player, i) => {
+        let type = (i % 2 == 0) ? 'hero' : 'villain';
+        return new Player(i, player, type); 
+        });  
     }
 
     // Display players in HTML
@@ -60,7 +76,6 @@ class Superwar {
         let fragment =
             this.buildPlayers('hero');
         team.append(fragment);
-
         team = document.getElementById('villains');
         team.innerHTML = '';
         fragment =
@@ -76,10 +91,7 @@ class Superwar {
             .forEach(player => fragment.append(player.view()));
         return fragment;
     }
-
 }
-
-
 window.onload = () => {
     const superwar = new Superwar(PLAYERS);
     superwar.viewPlayers();
